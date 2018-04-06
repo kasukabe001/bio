b55007e2efd608a06ddf61b450a2f5a3964cc48c
kfujita <kazuhiko.fujita@elife.co.jp>
2018-04-06 21:05:05 +0900

    hisashiburi

---
+++ asanotori_graph.php
@@ -0,0 +1,11 @@
+<?php
+
+
+?>
+<html>
+<div align="center">
+<IMG alt=朝の鳥 src="./images/jan2018.png" class="img-responsive" >
+</div>
+<iframe src="http://gairai.sakura.ne.jp/asa/index.php?_act=Tmonth&_mod=Data&myurl=fujita" name="sample" scrolling="yes" >
+</iframe>
+</html>
